ada31fc15c9f010180bed552d8ec400b4360f10b
aokiayukodesu <catch.you.later1031@gmail.com>
2023-02-05 23:20:42 +0900

    第7回課題プロジェクトのセットアップ

---
+++ src/test/java/com/Raisetech/Task7/original/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Raisetech.Task7.original;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
