340ae6f0479c087823ebe409a5989b0647f11d03
aokiayukodesu <catch.you.later1031@gmail.com>
2023-02-12 23:18:57 +0900

    フォーマットの修正

--- src/main/java/com/Raisetech/Task7/original/DobController.java
+++ src/main/java/com/Raisetech/Task7/original/DobController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DobController {
 
     @GetMapping("/dobs")
-    public String getUserByDateOfBirth(@Validated UserDateFormat form, BindingResult result, @RequestParam("date_of_birth") String dateOfBirth) {
+    public String getUserByDateOfBirth(@Validated UserDateFormat form, BindingResult result, @RequestParam("date_of_birth")
+    String dateOfBirth) {
         if (result.hasErrors()) {
         }
         return "20字以上の文字や空文字及び、yyy/MM/dd様式以外の生年月日は入力は出来ません";
